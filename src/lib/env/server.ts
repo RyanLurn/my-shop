313e0957f022825e69d50679c3dad3bdc9c7552a
Ryan Luong <lumintriet@gmail.com>
2025-11-05 20:53:40 +0700

    chore(identity): configure env variables for Better Auth

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 const serverEnv = createEnv({
   server: {
     DB_FILE_NAME: z.string().min(1),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.url(),
   },
   experimental__runtimeEnv: process.env,
 });
