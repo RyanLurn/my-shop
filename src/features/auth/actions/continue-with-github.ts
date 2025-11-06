16baf75bb896f5195124ec4681b09f9ba0a6cf2e
Ryan Luong <lumintriet@gmail.com>
2025-11-06 11:17:29 +0700

    feat(auth): add auth-specific error handler

--- src/features/auth/actions/continue-with-github.ts
+++ src/features/auth/actions/continue-with-github.ts
@@ -2,6 +2,7 @@
 
 import { headers } from "next/headers";
 import { auth } from "@/features/auth";
+import { handleAuthApiError } from "@/features/auth/helpers/handle-auth-api-error";
 import { getDictionary } from "@/features/i18n/helpers/get-dictionary";
 import { getDisplayLanguage } from "@/features/i18n/helpers/get-display-language";
 import type { UnexpectedError } from "@/types/errors";
@@ -21,12 +22,25 @@ async function continueWithGithub() {
 
     return { errors: [] };
   } catch (error) {
+    const handleAuthApiErrorResult = handleAuthApiError({
+      dictionary,
+      error,
+      context: {
+        location: "Continue with GitHub server action",
+        headers: webHeaders,
+      },
+    });
+    if (handleAuthApiErrorResult.isOk()) {
+      return { errors: [handleAuthApiErrorResult.value.message] };
+    }
+
     const errorMessage = dictionary.errors.unexpected.continueWithGithub;
     const errorLog: UnexpectedError = {
       kind: "unexpected",
       message: errorMessage,
       error,
       context: {
+        location: "Continue with GitHub server action",
         headers: webHeaders,
       },
     };
